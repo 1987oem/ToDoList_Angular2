feb483c83b8a081d657baabf3c6bef9a16b424e6
Ujin <1987oem@gmail.com>
2017-06-09 23:43:10 +0300

    Add files via upload

---
+++ source/js/factories/cardFactory.js
@@ -0,0 +1,47 @@
+angular.module('app').factory('cardFactory', function () {
+    var service = {};
+
+    var cards = [
+        {
+            id: 1,
+            description: 'Learn',
+            list_id: 1
+        },
+        {
+            id: 2,
+            description: 'Code',
+            list_id: 2
+
+        },
+        {
+            id: 3,
+            description: 'Fix bug',
+            list_id: 3
+
+        }
+    ];
+
+    service.getCards = function (list) {
+        return _.filter(cards, {list_id: list.id});
+    };
+
+    service.createCard = function (list, cardDescription) {
+        cards.push({
+           id: _.uniqueId('card_'),
+            description: cardDescription,
+            list_id: list.id
+        });
+    };
+
+    service.deleteCard = function (card) {
+        return _.pull(cards, card);
+    };
+
+    service.updateCard = function (updatingCard) {
+        var card = _.findWhere(cards, {id: updatingCard.id});
+        card.description = updatingCard.description;
+        card.list_id = updatingCard.list_id;
+    };
+
+    return service;
+});
